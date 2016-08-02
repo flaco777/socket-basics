9c71e456846350c675ea75f452c6e09b473ce554
Jared Mata <flaco777.jm@gmail.com>
2016-08-01 20:53:11 -0700

    Added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,7 @@ var socket = io();
 socket.on('connect', function () {
 	console.log('Connected to socket.io server');
 });
+
+socket.on('message', function (message) {
+	console.log('You have a new message: '+ message.text);
+})
